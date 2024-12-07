182912a7bc7ce142d5e4f88849d373c4420c6ce8
Martin Sturm <martin@martinsturm.nl>
2024-12-07 16:15:10 +0100

    Day6 solved

--- 6/6.py
+++ 6/6.py
@@ -4,93 +4,81 @@ from collections import defaultdict
 from http.cookiejar import debug
 
 file1 = '6.in'
+#file1 = '6.test'
+
 
 input = [x.strip() for x in open(file1, 'r').readlines()]
-O = {}
-i = 0 # North
-si = i
+SO = {}
+si = 0
 dir = [(-1,0), (0,1), (1,0), (0,-1)]
-cr, cc = 0,0
 sr, sc = 0,0
 R, C = len(input), len(input[0])
 for r in range(len(input)):
     for c in range(len(input[0])):
         if input[r][c] == '#':
-            O[(r,c)] = '#'
+            SO[(r,c)] = '#'
         elif input[r][c] == '^':
-            cr, cc = r,c
             sr, sc = r,c
 
-def has_loop(O):
-    cc, cr, i = sc, sr, si
-    visited = set()
-    visited_with_d = defaultdict(int)
-    while 0 <= cr+dir[i][0] < R and 0 <= cc+dir[i][1] < C:
-        if cr+dir[i][0] < 0 or cr+dir[i][0] >= R or cc+dir[i][1] < 0 or cc+dir[i][1] >= C: # not a loop, we go out of bounds
-            print("not a loop:", cr, cc, i)
-            break
-        # print(cr, cc, i)
-        visited.add((cr, cc))
-        visited_with_d[(cr, cc, i)] += 1
-        if visited_with_d[(cr, cc, i)] > 1:
-            print("loop:", cr, cc, i)
-            return True
-        if (cr+dir[i][0], cc+dir[i][1]) in O: #rotate
-            i += 1
-            if i >= len(dir):
-                i = 0
-        cr, cc = cr+dir[i][0], cc+dir[i][1]
-    if (cr, cc, i) in visited_with_d:
-        # print(cr, cc, i, visited_with_d)
-        return True
-    else:
-        # print("p1", len(visited))
-        return False
 
-p2 = 0
 visited = set()
-visited_with_d = set()
-obstacles_checked = set()
-positive_obstacles = set()
-while cr >= 0 and cc >= 0 and cr < R and cc < C:
-    # check for loop
-    if (cr+dir[i][0], cc+dir[i][1]) not in obstacles_checked:
-        if 0 <= cr+dir[i][0] < R and 0 <= cc+dir[i][1] < C:
-            obstacles_checked.add((cr+dir[i][0], cc+dir[i][1]))
-            OC = deepcopy(O)
-            OC[(cr+dir[i][0], cc+dir[i][1])] = '#'
-            if has_loop(OC):
-                p2+=1
-                positive_obstacles.add((cr+dir[i][0], cc+dir[i][1]))
-                print("loop", cr+dir[i][0], cc+dir[i][1], p2)
 
-    visited.add((cr, cc))
-    visited_with_d.add((cr, cc, i))
-    if cr+dir[i][0] < 0 or cr+dir[i][0] >= R or cc+dir[i][1] < 0 or cc+dir[i][1] >= C:
+O = SO
+i = si
+cr, cc = sr, sc
+while True:
+    # check for loop
+
+    dr, dc = dir[i]
+    if not (0 <= cr < R and 0 <= cc < C): # out of bounds
         break
-    elif (cr+dir[i][0], cc+dir[i][1]) in O: #rotate
+    elif (cr+dr, cc+dc) in O: #rotate
         i += 1
         if i >= len(dir):
             i = 0
+        dr, dc = dir[i]
 
-    # part 1
-    cr, cc = cr+dir[i][0], cc+dir[i][1]
+    visited.add((cr, cc))
+    cr, cc = cr+dr, cc+dc
+p1 = len(visited)    
 
-    # print(cr, cc)
+# part 2
+p2 = 0
+#for (nr, nc) in visited:
+for nr in range(R):
+#    if (nr%10)==0:
+#        print(nr, p2)
+    for nc in range(C):
+        visited_with_d = set()
+        i = si 
+        cr, cc = sr, sc
+        while True:
+            if (cr, cc, i) in visited_with_d:
+                p2+= 1
+                break
+            visited_with_d.add((cr, cc, i))
+            dr, dc = dir[i]
+            if not (0 <= cr < R and 0 <= cc < C): # out of bounds
+                break
+            elif (cr+dr, cc+dc) in O or cr+dr==nr and cc+dc==nc: #rotate
+                i += 1
+                if i >= len(dir):
+                    i = 0
+                dr, dc = dir[i]
+            else:
+                cr, cc = cr+dr, cc+dc
 
-for r in range(R):
-    for c in range(C):
-        if (r,c) in positive_obstacles:
-            print('O', end='')
-        elif (r,c) in visited:
-            print('X', end='')
-        elif (r,c) in O:
-            print('#', end='')
-        else:
-            print('.', end='')
-    print("")
-# print(visited)
-# has_loop(O)
-print(len(visited))
+# for r in range(R):
+#   for c in range(C):
+#        if (r,c) in positive_obstacles:
+#            print('O', end='')
+#        elif (r,c) in visited:
+#            print('X', end='')
+#        elif (r,c) in O:
+#            print('#', end='')
+#        else:
+#            print('.', end='')
+#    print("")
+
+print("p1", p1)
 print("p2", p2)
-print(positive_obstacles)
