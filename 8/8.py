322faad4990b3fc62b19007603cfdf983d374bf9
Martin Sturm <martin@martinsturm.nl>
2024-12-08 14:45:55 +0100

    Day 8 p2

--- 8/8.py
+++ 8/8.py
@@ -5,7 +5,8 @@ input = [x.strip() for x in open(file1, 'r').readlines()]
 A = {} 
 R = len(input)
 C = len(input[0])
-AN = {}
+AN1 = {}
+AN2 = {}
 for r, v in enumerate(input):
     for c,w in enumerate(v):
         if w != '.':
@@ -17,15 +18,42 @@ for (r1, c1) in A:
     # find other attenas with same freq:
     for (r2, c2) in A:
         if r2 != r1 and c2 != c1 and A[(r1,c1)] == A[(r2,c2)]:
+      #      print("pair", (r1,c1), (r2,c2), A[(r1, c1)])
+            AN2[(r1,c1)] = '#'
+            AN2[(r2,c2)] = '#'
+            rr1 = r1-r2
+            rr2 = r2-r1
+            cc1 = c1-c2
+            cc2 = c2-c1 
+            
             n1 = (r1+r1-r2,c1+c1-c2)
-            n2 = (r2+r2-r1,c2+c2-c1)
             nr1, nc1 = n1
+            n2 = (r2+r2-r1,c2+c2-c1)
             nr2, nc2 = n2
-            print("pair", (r1,c1), (r2,c2), A[(r1, c1)])
             if 0 <= nr1 < R and 0 <= nc1 < C:
-                print('antidote', n1)
-                AN[n1]='#'
+                AN1[n1]='#'
             if 0 <= nr2 < R and 0 <= nc2 < C:
-                print('antidote', n2)
-                AN[n2]='#'
-print(len(AN))            
+                AN1[n2]='#'
+
+            while 0 <= nr1 < R and 0 <= nc1 < C:
+                AN2[n1]='#'
+                n1 = (nr1+r1-r2,nc1+c1-c2)
+                nr1, nc1 = n1
+                
+            while 0 <= nr2 < R and 0 <= nc2 < C:
+                AN2[n2]='#'
+                
+                n2 = (nr2+r2-r1,nc2+c2-c1)
+                nr2, nc2 = n2
+print('p1', len(AN1))            
+print('p2', len(AN2))            
+exit(1)
+for r in range(R):
+    for c in range(C):
+        if (r, c) in A:
+            print(A[(r,c)], end='')
+        elif (r, c) in AN:
+            print('#', end='')
+        else: 
+            print('.',end='')
+    print('') 
