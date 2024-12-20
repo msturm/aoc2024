8be5526fc7103d1b7bf368f305415bcff763993d
Martin Sturm <martin.sturm@adevinta.com>
2024-12-20 10:04:38 +0100

    Day 16 complete

--- 16/16.py
+++ 16/16.py
@@ -3,7 +3,7 @@ from collections import deque
 import time
 from queue import PriorityQueue
 file1 = '16.in'
-file1 = '16.test'
+#file1 = '16.test'
 
 #input = [x.strip() for x in open(file1, 'r').readlines()]
 input = open(file1, 'r').read().strip()
@@ -27,22 +27,11 @@ class bcolors:
     UNDERLINE = '\033[4m'
     CLEAR = '\033[H'
 
-print(R,C)
 for r in range(R):
     for c in range(C):
-#        for d, (dr, dc) in enumerate(D):
-#            rr = r+dr
-#            cc = c+dc
-#            if G[r][c] == '.':
-#                COST[(r, c, d)] = INF
             if G[r][c] == 'S':
-#                if d == 1:
-#                    COST[(r, c, d)] = 0
-#                else:
-#                    COST[(r, c, d)] = INF
                 start = (r, c, 1)
             elif G[r][c] == 'E':
-#                COST[(r, c, d)] = INF
                 end = (r,c)
 
 #def printgrid(G, COST):
@@ -58,7 +47,7 @@ for r in range(R):
     print("")
 
 def printseen(G, SEEN):
-    print(bcolors.CLEAR, end ="")
+    #print(bcolors.CLEAR, end ="")
     for r in range(len(G)):
         for c in range(len(G[0])):
             if (r, c) in SEEN:
@@ -66,7 +55,6 @@ def printseen(G, SEEN):
             else:
                 print(G[r][c], end="")
         print("") 
-#        print("") 
             
     print("")
     time.sleep(0.2)
@@ -79,82 +67,76 @@ COST = dict()
 PQ = PriorityQueue()
 PQ.put((0, start[0], start[1], start[2]))
 SEEN = set()
-counter = 0
-#print(COST)
 p1_node = (INF,0,0,0)
 while not PQ.empty():
-    counter += 1
     cur_cost, r, c, d = PQ.get()
-#    print(v, node)
-    #r, c, d = node 
+#    print(cur_cost, r, c, d)
     if (r, c) == end:
         if p1_node[0] > cur_cost: 
             p1_node = (cur_cost, r, c, d)
-        break
-    #cur_cost = COST[(r, c, d)]
-    for dd, (dr, dc) in enumerate(D):
-        rr, cc = r + dr, c + dc
-        if dd == d and G[rr][cc] != '#' and (rr, cc, d) not in SEEN: # neighbour in same dir
-            SEEN.add((rr, cc, d))
-            new_cost = cur_cost + 1
-            COST[(rr, cc, d)] = new_cost
-            PQ.put((new_cost, rr, cc, d))
-#            print('1put', new_cost, cur_cost, rr, cc, d)
-        elif (((d-1)%4) == dd or dd == ((d+1)%4)) and G[rr][cc] != '#' and (r, c, dd) not in SEEN: # change of dir
-            SEEN.add((r, c, dd))
-            new_cost = cur_cost + 1000
-            COST[(r, c, dd)] = new_cost
-            PQ.put((new_cost, r, c, dd))
-#            print('2put', new_cost, r, c, dd)
-#            print('cupdate', r, c, new_cost, d)
-#    printseen(G, SEEN)
-#    printgrid(G, COST)
+#        break
+    if (r, c, d) not in COST:
+        COST[(r, c, d)] = cur_cost
+    if (r, c, d) in SEEN:
+        continue
+    SEEN.add((r, c, d))
+    dr, dc = D[d]
+    rr, cc = r + dr, c + dc
+    if G[rr][cc] != '#':
+        new_cost = cur_cost + 1
+        PQ.put((new_cost, rr, cc, d))
+    new_cost = cur_cost + 1000
+    PQ.put((new_cost, r, c, (d+1)%4))
+    PQ.put((new_cost, r, c, (d-1)%4))
+
 print('\033[?25h', end="")
 p1 = p1_node[0]
 print(p1_node)
 
-PQ = PriorityQueue()
 COST2 = dict()
-PQ.put((0, p1_node[1], p1_node[2], p1_node[3]))
-p1_cost = p1_node[0]
+PQ = PriorityQueue()
+for di in range(len(D)):
+    PQ.put((0, end[0], end[1], di))
 SEEN = set()
 while not PQ.empty():
-    counter += 1
     cur_cost, r, c, d = PQ.get()
-#    print(v, node)
-    #r, c, d = node 
-    if r == start[0] and c == start[1]:
-        break
-    #cur_cost = COST[(r, c, d)]
-    for dd, (dr, dc) in enumerate(D):
-         
-        rr, cc = r + dr, c + dc
-        if (dd-2)%4 == d and G[rr][cc] != '#' and (rr, cc, d) not in SEEN: # neighbour in same dir
-            SEEN.add((rr, cc, d))
-            new_cost = cur_cost + 1
-            COST2[(rr, cc, d)] = new_cost
-            PQ.put((new_cost, rr, cc, d))
-#            print('1put', new_cost, cur_cost, rr, cc, d)
-        elif (((d-1)%4) == dd or dd == ((d+1)%4)) and G[rr][cc] != '#' and (r, c, dd) not in SEEN: # change of dir
-            SEEN.add((r, c, dd))
-            new_cost = cur_cost + 1000
-            COST2[(r, c, (dd-2)%4)] = new_cost
-            PQ.put((new_cost, r, c, (dd-2)%4))
-        
-print('c', COST)
-print('c2', COST2)
+#    print(cur_cost, r, c, d)
+#    if (r, c) == (start[0], start[1]):
+#        break
+    if (r, c, d) not in COST2:
+        COST2[(r, c, d)] = cur_cost
+    if (r, c, d) in SEEN:
+        continue
+    SEEN.add((r, c, d))
+    dr, dc = D[(d-2)%4]
+    rr, cc = r + dr, c + dc
+    if G[rr][cc] != '#': 
+        new_cost = cur_cost + 1
+        print('adding', rr,cc,d, new_cost)
+        PQ.put((new_cost, rr, cc, d))
+    new_cost = cur_cost + 1000
+    PQ.put((new_cost, r, c, (d+1)%4))
+    PQ.put((new_cost, r, c, (d-1)%4))
+
+print("cost", COST)
+print("")
+print("cost2", COST2)
+
+p1_cost = p1_node[0]
 p2_s = set()
 for r in range(R):
     for c in range(C):
-        for dir in range(4):
+        for d in range(4):
+            if (r, c, d) in COST and (r, c, d) in COST2:
+                print(r, c, d, COST[(r, c, d)], COST2[(r, c, d)], p1_cost)
             if (r, c, d) in COST and (r, c, d) in COST2 and COST[(r, c, d)] + COST2[(r, c, d)] == p1_cost:
                 p2_s.add((r, c))
                 
  
-print("print R", p2_s) 
 printseen(G, p2_s)
 #printgrid(G, COST)
 print('p1', p1)
 print('p2', len(p2_s))
+print(p2_s)
                 
     
